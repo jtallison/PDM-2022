3b8a1234f49db8714a397c51b24f0c6ba35f69d7
Jesse Allison <jtallison@lsu.edu>
2022-02-15 12:49:58 -0600

    Made Sound lesson number 1

--- sound/sound-lesson-1/js/sketch.js
+++ sound/sound-lesson-1/js/sketch.js
@@ -1,7 +1,55 @@
+// Jesse Allison 2022
+// global variables
+
+const sounds = new Tone.Players({
+  shot: "media/02-shot.mp3",
+  through: "media/03-through.mp3",
+  theHeart: "media/04-theHeart.mp3"
+})
+let button1;
+let button2;
+let button3;
+
+let slider1;
+
+sounds.toDestination();
+
+function preload() {
+
+}
+
 function setup() {
   createCanvas(400, 400);
+
+  button1 = createButton("Shot", 'shot');
+  button1.position(200, 300);
+  button1.mousePressed(buttonSound);
+  
+  button2 = createButton("through");
+  button2.position(200, 340);
+  button2.mousePressed( () => buttonSound('through') );
+
+  button3 = document.getElementById('heartDiv');
+  button3.onclick = () => buttonSound('theHeart');
+
+  slider1 = createSlider(0,1,0,0.1);
 }
 
 function draw() {
   background(220);
 }
+
+function keyPressed(){
+  if(key==="1"){
+    sounds.player("shot").start();
+  } else if (key === "2"){
+    sounds.player("through").start();
+  } else if (key === "3") {
+    sounds.player("theHeart").start();
+  }
+}
+
+function buttonSound(sound='shot') {
+  sounds.player(sound).start();
+}
+
