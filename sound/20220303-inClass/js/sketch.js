c8f4bf1ce40f4d394fdd2165173a64a6d01afd77
Jesse Allison <jtallison@lsu.edu>
2022-03-03 15:03:19 -0600

    after class update

--- sound/20220303-inClass/js/sketch.js
+++ sound/20220303-inClass/js/sketch.js
@@ -1,7 +1,9 @@
 // Jesse Allison 2022
 
+let pitch = 800;
+
 // Set up Tone
-let osc = new Tone.AMOscillator(600,'sine','sine').start()
+let osc = new Tone.AMOscillator(pitch,'sine','sine').start()
 let gain = new Tone.Gain().toDestination();
 let pan = new Tone.Panner().connect(gain);
 let ampEnv = new Tone.AmplitudeEnvelope({
@@ -12,7 +14,19 @@ let ampEnv = new Tone.AmplitudeEnvelope({
 }).connect(pan);
 osc.connect(ampEnv);
 
+let freqLFO = new Tone.LFO(4,300,1000).start();
+freqLFO.connect(osc.frequency); 
 
+
+let noise = new Tone.Noise('pink').start();
+let noiseEnv = new Tone.AmplitudeEnvelope({
+  attack: 0.1,
+  decay: 0.2,
+  sustain: 1.0,
+  release: 0.8
+}).connect(gain);
+let noiseFilter = new Tone.Filter(800,'lowpass').connect(noiseEnv);
+noise.connect(noiseFilter);
 
 
 function setup() {
@@ -21,8 +35,23 @@ function setup() {
 
 function draw() {
   background(220);
+
+  // console.log(frameCount);
+  // if((frameCount % 60) === 0 ) {
+  //   pitch = random(300, 1000);
+  // }
+
 }
 
 function mousePressed() {
   console.log('pressed');
+  ampEnv.triggerAttackRelease('4n');
+  // osc.frequency.linearRampTo(400,'+1');
+  osc.frequency.linearRampToValueAtTime(pitch-200,'+1');
+  ampEnv.triggerAttackRelease('4n','+1');
+
+  if(mouseY > 400) {
+    noiseFilter.frequency.value = mouseX +100;
+    noiseEnv.triggerAttackRelease(0.5);
+  }
 }
