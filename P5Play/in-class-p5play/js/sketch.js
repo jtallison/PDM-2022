262a9105d88369379a7fcf18cf6166cfb08c4bca
Jesse Allison <jtallison@lsu.edu>
2022-04-21 17:38:09 -0500

    Meteorites from JSON

--- P5Play/in-class-p5play/js/sketch.js
+++ P5Play/in-class-p5play/js/sketch.js
@@ -1,6 +1,19 @@
 
 let sprites = [];
 
+let meteors;
+
+function preload() {
+
+  fetch("https://data.nasa.gov/resource/y77d-th95.json")
+  .then(res => res.json())
+  .then(out => {
+    console.log("Got Metorites")
+    meteors = out;
+  })
+
+}
+
 function setup() {
   createCanvas(800,400);
   createSprite(400, 200, 50, 50);
@@ -12,7 +25,17 @@ function draw() {
 }
 
 function mousePressed() {
-  sprites[sprites.length] = createSprite(random(width), random(height), 10,10)
+  // sprites[sprites.length] = createSprite(random(width), random(height), 10,10)
+  let met = meteors[sprites.length];
+  let metX = map(met.reclat, -180, 180, 0, width);
+  let metY = map(met.reclong, -180, 180, 0, height);
+  let metMass;
+  if(met.mass) {
+    metMass = map(met.mass, 0, 5000, 10, 100);
+  } else {
+    metMass = map(5, 0, 5000, 10, 100);
+  }
+  sprites[sprites.length] = createSprite(metX, metY, metMass, metMass);
 }
 
 function moveDown(distance=2) {
